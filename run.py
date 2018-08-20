d9bb3d8ea84ca42cd5a7ce5526980032ee5e33a5
Nicolas Zambrano <30539056+Niki0825@users.noreply.github.com>
2018-08-20 16:36:28 -0500

    Update run.py

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen("https://scholar.google.es/citations?user=GlM971cAAAAJ&hl=es&oi=ao") #here puts the url of the page
-bsObj = BeautifulSoup(html)
+bsObj = BeautifulSoup(html,"html.parser")
 content_all = bsObj.findAll("table", {"id": "gsc_a_t"}) #find the father structure that contains your info
 info = bsObj.findAll("tr") # specify what find in the structure to save
-
+grafica = bsObj.findAll("div", {"class": "gsc_md_hist_b"})
 csvFile = open("prueba2.csv", "wt", newline='') #create the csv file to save the info
 writer = csv.writer(csvFile)
 try:
@@ -15,5 +15,11 @@ try:
         for cell in i.findAll(['td', 'th']):
             csvRow.append(cell.get_text()) #and put it in a list
             writer.writerow(csvRow)
+
+    for año in grafica:
+        csvRow.append(año.get_text())
+        writer.writerow(csvRow)
 finally:
     csvFile.close()
+
+
